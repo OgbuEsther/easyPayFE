f8b482428dfddda2c6e94c50c7dc349fd130d4ac
Ovohkay <estherovuokeraye@gmail.com>
2023-03-24 16:24:40 +0100

    api consumption on staff

--- src/Components/Dashboards/Lists.tsx
+++ src/Components/Dashboards/Lists.tsx
@@ -1,239 +1,248 @@
-import React from 'react'
-import styled from 'styled-components'
-import img from "../Assets/21.jpeg"
-
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import styled from "styled-components";
+import img from "../Assets/21.jpeg";
+import { getAllClients } from "../api/staffEndpoints";
+import { UseAppSelector } from "../Global/Store";
 const Recent = () => {
+  const allClients = useQuery({
+    queryKey: ["viewClients"],
+    queryFn: getAllClients,
+  });
+  console.log("this is all staffs", allClients.data);
+  const user = UseAppSelector((state) => state.Client);
+
+  console.log(user);
   return (
-      <Container>
-          <Top><h2>List of Staffs</h2></Top>
-          <Boxhold>
-              
-              
-          </Boxhold>
-          <Head>
-                  <Navs>Staff</Navs>
-                  <Navs>Role</Navs>
-                  <Navs>Joined</Navs>
-            </Head>
-              <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+    <Container>
+      <div>
+        <Top>
+          <h2>List of Staffs</h2>
+        </Top>
+        <Boxhold></Boxhold>
+        <Head>
+          <Navs>Staff</Navs>
+          <Navs>Role</Navs>
+          <Navs>Joined</Navs>
+        </Head>
+        {allClients?.data?.data?.map((props: any) => (
+          <Head2 key={props._id}>
+            <Profilehold>
+              <Circle>
+                <Img src={img} />
+              </Circle>
+              <Name>{props.yourName}</Name>
+            </Profilehold>
 
-              <Role>
-                  Supervisor
-              </Role>
+            <Role>{props.position}</Role>
 
-              <Joined>10/12/09</Joined>
+            <Joined>10/12/09</Joined>
           </Head2>
+        ))}
+        {/* <Head2>
+          <Profilehold>
+            <Circle>
+              <Img src={img} />
+            </Circle>
+            <Name>Ogbu Esther</Name>
+          </Profilehold>
 
-          <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+          <Role>Supervisor</Role>
 
-              <Role>
-                  Supervisor
-              </Role>
+          <Joined>10/12/09</Joined>
+        </Head2> */}
 
-              <Joined>10/12/09</Joined>
-          </Head2>
+        {/* <Head2>
+          <Profilehold>
+            <Circle>
+              <Img src={img} />
+            </Circle>
+            <Name>Ogbu Esther</Name>
+          </Profilehold>
 
-          <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+          <Role>Supervisor</Role>
 
-              <Role>
-                  Supervisor
-              </Role>
+          <Joined>10/12/09</Joined>
+        </Head2>
+        <Head2>
+          <Profilehold>
+            <Circle>
+              <Img src={img} />
+            </Circle>
+            <Name>Ogbu Esther</Name>
+          </Profilehold>
 
-              <Joined>10/12/09</Joined>
-          </Head2>
-          <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+          <Role>Supervisor</Role>
 
-              <Role>
-                  Supervisor
-              </Role>
+          <Joined>10/12/09</Joined>
+        </Head2> */}
+        {/* <Head2>
+          <Profilehold>
+            <Circle>
+              <Img src={img} />
+            </Circle>
+            <Name>Ogbu Esther</Name>
+          </Profilehold>
 
-              <Joined>10/12/09</Joined>
-          </Head2>
-          <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+          <Role>Supervisor</Role>
 
-              <Role>
-                  Supervisor
-              </Role>
+          <Joined>10/12/09</Joined>
+        </Head2>
+        <Head2>
+          <Profilehold>
+            <Circle>
+              <Img src={img} />
+            </Circle>
+            <Name>Ogbu Esther</Name>
+          </Profilehold>
 
-              <Joined>10/12/09</Joined>
-          </Head2>
-          <Head2>
-              <Profilehold>
-                  <Circle><Img src={ img} /></Circle>
-                  <Name>Ogbu Esther</Name>
-              </Profilehold>
+          <Role>Supervisor</Role>
 
-              <Role>
-                  Supervisor
-              </Role>
-
-              <Joined>10/12/09</Joined>
-          </Head2>
+          <Joined>10/12/09</Joined>
+        </Head2> */}
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Recent
+export default Recent;
 const Button = styled.div`
-    width: 120px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    background-color: #03993F;
-    border-radius: 50px;
-    cursor: pointer;
-`
+  width: 120px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  background-color: #03993f;
+  border-radius: 50px;
+  cursor: pointer;
+`;
 const View = styled.div`
-    width: 100%;
-    display: flex;
-    margin-top: 30px;
-    justify-content: center;
-    align-items: center;
-`
-const Joined = styled.div`
-    
-`
+  width: 100%;
+  display: flex;
+  margin-top: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+const Joined = styled.div``;
 const Role = styled.div`
-    width: 200px;
-    /* background-color: green; */
-    margin-right: 90px;
-    display: flex;
-    justify-content: center;
-    @media screen and (max-width: 500px) {
-        background-color: red;
-        margin-right: 12px;
-        /* margin-left: 30px; */
-        width: 0px;
-    }
-`
+  width: 200px;
+  /* background-color: green; */
+  margin-right: 90px;
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 500px) {
+    background-color: red;
+    margin-right: 12px;
+    /* margin-left: 30px; */
+    width: 0px;
+  }
+`;
 const Img = styled.img`
-    width: 100px;
-    height: 100px;
-    object-fit: cover;
-`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+`;
 const Name = styled.div`
-    display: flex;
-    margin-left: 10px;
-    @media screen and (max-width: 500px){
-        width: 50px;
-        /* background-color: red; */
-    }
-    
-`
+  display: flex;
+  margin-left: 10px;
+  @media screen and (max-width: 500px) {
+    width: 50px;
+    /* background-color: red; */
+  }
+`;
 const Circle = styled.div`
-    width: 45px;
-    height: 45px;
-    border: 1px solid #fff;
-    /* background-color: red; */
-    display: flex;
-    /* justify-content */
-    align-items: center;
-    border-radius: 50px;
-    overflow: hidden;
-    @media screen and (max-width: 500px) {
-        width: 40px;
-        height: 40px;
-        border-radius: 50px;
-    }
-`
-const Profilehold = styled.div`
-    display: flex;
-    align-items: center;
-`
-const Hold = styled.div`
+  width: 45px;
+  height: 45px;
+  border: 1px solid #fff;
+  /* background-color: red; */
+  display: flex;
+  /* justify-content */
+  align-items: center;
+  border-radius: 50px;
+  overflow: hidden;
+  @media screen and (max-width: 500px) {
+    width: 40px;
     height: 40px;
-    width: 100%;
-    display: flex;
-    margin-top: 10px;
-    justify-content: space-between;
-    background-color: blue;
-    align-items: center;
-`
+    border-radius: 50px;
+  }
+`;
+const Profilehold = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Hold = styled.div`
+  height: 40px;
+  width: 100%;
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-between;
+  background-color: blue;
+  align-items: center;
+`;
 const Navs2 = styled.div`
-    color: #5a5a5a;
-    font-size: 18px;
-`
+  color: #5a5a5a;
+  font-size: 18px;
+`;
 const Navs = styled.div`
-    color: #fff;
-    font-size: 18px;
-    cursor: pointer;
-`
+  color: #fff;
+  font-size: 18px;
+  cursor: pointer;
+`;
 const Head2 = styled.div`
-    background-color: #F6F7F9;
-    height: 80px;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 20px;
-    display: flex;
-    /* height: 100%; */
-    /* height: 60px; */
-    padding-left: 10px;
-    padding-right: 10px;
-`
+  background-color: #f6f7f9;
+  height: 80px;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  display: flex;
+  /* height: 100%; */
+  /* height: 60px; */
+  padding-left: 10px;
+  padding-right: 10px;
+`;
 const Head = styled.div`
-    margin-top: 15px;
-    justify-content: space-between;
-    background-color: #0D71FA;
-    align-items: center;
-    display: flex;
-    height: 70px;
-    padding-left: 15px;
-    padding-right: 15px;
-    position: sticky;
-    border-radius: 10px;
-`
+  margin-top: 15px;
+  justify-content: space-between;
+  background-color: #0d71fa;
+  align-items: center;
+  display: flex;
+  height: 70px;
+  padding-left: 15px;
+  padding-right: 15px;
+  position: sticky;
+  border-radius: 10px;
+`;
 const Boxhold = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    
-    /* background-color: red; */
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  /* background-color: red; */
+`;
 const Top = styled.div`
-    h2{
-        margin: 0;
-        font-size: 20px;
-    }
-`
+  h2 {
+    margin: 0;
+    font-size: 20px;
+  }
+`;
 
 const Container = styled.div`
-    width: 93%;
-    display: flex;
-    height: 100%;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    padding: 30px;
-    margin-top: 30px;
-    flex-direction: column;
-    background-color: #fff;
-    /* background-color: black; */
-    margin-left: 260px;
-    @media screen and (max-width: 500px) {
-        padding: 20px;
-    }
-    @media screen and (max-width: 1024px) {
-        margin-left: 0px;
-    }
-`
+  width: 93%;
+  display: flex;
+  height: 100%;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 30px;
+  margin-top: 30px;
+  flex-direction: column;
+  background-color: #fff;
+  /* background-color: black; */
+  margin-left: 260px;
+  @media screen and (max-width: 500px) {
+    padding: 20px;
+  }
+  @media screen and (max-width: 1024px) {
+    margin-left: 0px;
+  }
+`;
