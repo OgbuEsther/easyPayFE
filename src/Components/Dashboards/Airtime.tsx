69b916ef16b0f1b42548b03ce209ec42e61ff4da
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-09 09:26:57 +0100

    just pushed

--- src/Components/Dashboards/Airtime.tsx
+++ src/Components/Dashboards/Airtime.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import img from "../Assets/airtel.jpg"
+import img2 from "../Assets/mtn.jpg"
+import img3 from "../Assets/glo.jpg"
 
 interface Props {
   isOpen: boolean;
@@ -13,18 +16,128 @@ const Popup: React.FC<Props> = ({ isOpen }) => {
   return (
     <Popups>
       <Popup_content>
-        {/* insert your popup content here */}
+              <h3>Payments</h3>
+              <p>Select the service you want to make payment for</p>
+              <h4>Data Services</h4>
+              <Box>
+                  <Airtel>
+                      <Img src={img} />
+                      <AirtelText>
+                          <strong>Airtel Data</strong>
+                          <Text>Airtel Data - Get instant top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img2} />
+                      <AirtelText>
+                          <strong>MTN Data</strong>
+                          <Text>MTN Data - Get instant Data Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img3} />
+                      <AirtelText>
+                          <strong>GLO Data</strong>
+                          <Text>GLO Data - Get instant Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img3} />
+                      <AirtelText>
+                          <strong>GLO Data</strong>
+                          <Text>GLO Data - Get instant Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img3} />
+                      <AirtelText>
+                          <strong>GLO Data</strong>
+                          <Text>GLO Data - Get instant Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img3} />
+                      <AirtelText>
+                          <strong>GLO Data</strong>
+                          <Text>GLO Data - Get instant Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+
+                  <Airtel>
+                      <Img src={img3} />
+                      <AirtelText>
+                          <strong>GLO Data</strong>
+                          <Text>GLO Data - Get instant Top up</Text>
+                      </AirtelText>
+                  </Airtel>
+              </Box>
       </Popup_content>
     </Popups>
   );
 };
 
 export default Popup;
+const Text = styled.div`
+    font-size: 11px;
+`
+const AirtelText = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+    strong{
+        margin: 0;
+        font-weight: 500;
+        font-size: 15px;
+    }
+`
+const Img = styled.img`
+    height: 50px;
+`
+const Airtel = styled.div`
+    width: 250px;
+    height: 50px;
+    border: 1px solid #CEC2C2;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    margin-bottom: 10px;
+`
+const Box = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    /* background-color: green; */
+`
 const Popup_content = styled.div`
+    width: 850px;
+    height: 450px;
     background-color: white;
-  padding: 20px;
+    display: flex;
+    flex-direction: column;
+  padding: 25px;
   border-radius: 5px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  h3{
+    margin: 0px;
+    color: #173D52 !important;
+    font-size: 1.3em;
+    font-weight: 500;
+  }
+  p{
+    font-size: 15px;
+    margin-top: 7px;
+  }
+  h4{
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.3;
+  }
 `
 
 const Popups = styled.div`
